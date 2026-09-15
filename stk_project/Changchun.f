789d3a667902e45224d5b023dc9a1115ddc9da0e
mimi <3322346701@qq.com>
2026-09-15 13:10:11 +0800

    Add files via upload

---
+++ stk_project/Changchun.f
@@ -0,0 +1,167 @@
+stk.v.9.0
+WrittenBy    STK_v9.2.1
+
+BEGIN Facility
+
+Name        Changchun
+
+	BEGIN CentroidPosition
+
+		CentralBody            Earth
+		DisplayCoords          Geodetic
+		EcfLatitude            4.38800000000000e+001
+		EcfLongitude           1.25322778000000e+002
+		EcfAltitude            0.00000000000000e+000
+		HeightAboveGround      0.00000000000000e+000
+		DisplayAltRef          Ellipsoid
+		UseTerrainInfo         Off
+		NumAzRaysInMask        360
+		TerrainNormalMode      UseCbShape
+
+	END CentroidPosition
+
+BEGIN Extensions
+    
+    BEGIN Graphics
+
+            BEGIN Attributes
+
+                MarkerColor             #ff0000
+                LabelColor              #ff0000
+                LineStyle               0
+                MarkerStyle             9
+                FontStyle               0
+
+            END Attributes
+
+            BEGIN Graphics
+
+                Inherit                 On
+                IsDynamic               Off
+                ShowLabel               On
+                ShowAzElMask            Off
+                ShowAzElFill            Off
+                AzElFillStyle           7
+                UseAzElColor               Off
+                AzElColor               #ffffff
+                MinDisplayAlt           0.000
+                MaxDisplayAlt           10000000.000
+                NumAzElMaskSteps        1
+                ShowAzElAtRangeMask       Off
+                ShowAzElAtRangeFill       Off
+                AzElAtRangeFillStyle      7
+                UseAzElAtRangeColor          Off
+                AzElAtRangeColor          #ffffff
+                MinDisplayRange           0.000
+                MaxDisplayRange           10000000.000
+                NumAzElAtRangeMaskSteps   1
+
+            BEGIN RangeContourData
+                    Show                 Off
+                    ShowRangeFill        Off
+                    RangeFillStyle       7
+                    LabelUnits           4
+                    NumDecimalDigits     3
+
+            END RangeContourData
+
+            END Graphics
+    END Graphics
+    
+    BEGIN LaserCAT
+    END LaserCAT
+    
+    BEGIN ExternData
+    END ExternData
+    
+    BEGIN RFI
+    END RFI
+    
+    BEGIN ADFFileData
+    END ADFFileData
+    
+    BEGIN AccessConstraints
+		LineOfSight   IncludeIntervals 
+		ElevationAngle		Min  5.0000000000e+000   IncludeIntervals 
+    END AccessConstraints
+    
+    BEGIN ObjectCoverage
+    END ObjectCoverage
+    
+    BEGIN Desc
+        LongText    166
+City Name		Changchun
+Type			2
+Province		Jilin
+Country			CHINA
+Province Rank		
+Population		
+Latitude		43.88 deg
+Longitude		125.322778 deg
+Central Body		Earth
+
+    END Desc
+    
+    BEGIN Atmosphere
+<!-- STKv4.0 Format="XML" -->
+<STKOBJECT>
+<OBJECT Class = "AtmosphereExtension" Name = "STK_Atmosphere_Extension">
+    <OBJECT Class = "link" Name = "AtmosAbsorptionModel">
+        <OBJECT Class = "AtmosphericAbsorptionModel" Name = "Simple_Satcom">
+            <OBJECT Class = "bool" Name = "Clonable"> True </OBJECT>
+            <OBJECT Class = "string" Name = "Description"> &quot;Simple Satcom gaseous absorption model&quot; </OBJECT>
+            <OBJECT Class = "bool" Name = "ReadOnly"> False </OBJECT>
+            <OBJECT Class = "double" Name = "SurfaceTemperature"> 293.15 K </OBJECT>
+            <OBJECT Class = "string" Name = "Type"> &quot;Simple Satcom&quot; </OBJECT>
+            <OBJECT Class = "string" Name = "UserComment"> &quot;Simple Satcom gaseous absorption model&quot; </OBJECT>
+            <OBJECT Class = "string" Name = "Version"> &quot;1.0.0 a&quot; </OBJECT>
+            <OBJECT Class = "double" Name = "WaterVaporConcentration"> 7.5 g*m^-3 </OBJECT>
+        </OBJECT>
+    </OBJECT>
+    <OBJECT Class = "bool" Name = "Clonable"> True </OBJECT>
+    <OBJECT Class = "string" Name = "Description"> &quot;STK Atmosphere Extension&quot; </OBJECT>
+    <OBJECT Class = "bool" Name = "EnableLocalRainData"> False </OBJECT>
+    <OBJECT Class = "bool" Name = "InheritAtmosAbsorptionModel"> True </OBJECT>
+    <OBJECT Class = "double" Name = "LocalRainIsoHeight"> 2000 m </OBJECT>
+    <OBJECT Class = "double" Name = "LocalRainRate"> 1 mm*hr^-1 </OBJECT>
+    <OBJECT Class = "double" Name = "LocalSurfaceTemp"> 293.15 K </OBJECT>
+    <OBJECT Class = "bool" Name = "ReadOnly"> False </OBJECT>
+    <OBJECT Class = "string" Name = "Type"> &quot;STK Atmosphere Extension&quot; </OBJECT>
+    <OBJECT Class = "string" Name = "UserComment"> &quot;STK Atmosphere Extension&quot; </OBJECT>
+    <OBJECT Class = "string" Name = "Version"> &quot;1.0.0 a&quot; </OBJECT>
+</OBJECT>
+</STKOBJECT>
+    END Atmosphere
+    
+    BEGIN RCS
+	Inherited          True
+	LinearClutterCoef        1.000000e+000
+	BEGIN RCSBAND
+		LinearConstantValue      1.000000e+000
+		Swerling      0
+		BandData      3.000000e+006 3.000000e+011
+	END RCSBAND
+    END RCS
+    
+    BEGIN DisplayTm
+		DisplayGT	AlwaysOff
+		DisplayType	AlwaysOn
+    END DisplayTm
+    
+    BEGIN Identification
+    END Identification
+    
+    BEGIN Crdn
+    END Crdn
+    
+    BEGIN VO
+    END VO
+
+END Extensions
+
+BEGIN SubObjects
+
+END SubObjects
+
+END Facility
+
